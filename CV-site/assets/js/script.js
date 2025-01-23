672ce7f78644d915559bd2be0caae88cc4454734
Ito <itocode21@gmail.com>
2025-01-23 06:04:47 +0300

    Delete CV-site directory

--- CV-site/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('main section');
-     sections.forEach((section, index) => {
-            section.style.animationDelay = `${index * 0.1}s`;
-       });
-});
